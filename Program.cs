4c06ca94550ea65b9d54fba88ec6d771663b584e
Ahmed Fathy <ahmedfathy11876@gmail.com>
2026-06-21 18:37:46 +0300

    Update the program file using the middleware
    Signed-off-by: Ahmed Fathy <ahmedfathy11876@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using CRM.Common.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("localappsettings.json", optional: true, reloadOnChange: true);
@@ -5,6 +7,8 @@ builder.Configuration.AddJsonFile("localappsettings.json", optional: true, reloa
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
